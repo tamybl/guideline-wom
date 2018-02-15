e4b5aae35d8a13bcefd411b6af761537be7516ac
tamybl <tamara.bravo.l@gmail.com>
2018-02-15 20:56:51 -0300

    Correccion menu interactivo y edicion de contenido

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@ $(document).ready( function () {
   $('.wm-nav ul li').click(function (e) {
     e.preventDefault();
     var href = $(this).find('a').attr('href');
-    //$(this).find('a').css('background-color', '#000');
-    if ($(href).offset().top !== 0) {
-      $(".wm-content").animate({ scrollTop: $(href).offset().top }, 1000);
-    }
-    
+    $(".wm-content").stop().animate({ scrollTop: $(href).position().top }, 1000);
+
+    $('.wm-nav ul').find('li').removeClass('active');
+        $('.wm-nav ul li').find('[href="'+href+'"]').parent().addClass('active');
   })
+  /*$('.wm-content .wm-text').scroll( function () {
+    if ($(this).find(id).position() <= 33) {
+      console.log('Estoy en la seccion'+href);
+    }
+  })*/
 
 
   
