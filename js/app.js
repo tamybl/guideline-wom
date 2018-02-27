c01ae7c602665a30dee58268b80d61bbf8995339
tamybl <tamara.bravo.l@gmail.com>
2018-02-27 15:24:50 -0300

    Correccion media query

--- js/app.js
+++ js/app.js
@@ -37,13 +37,12 @@ $(document).ready( function () {
   })
 })
 
-
 function scrollSection () {
   var scrollPosition = $(document).scrollTop();
   $('.wm-content article').each(function () {
     var idTop = $(this).offset().top;
     var idBottom = ($(this).height())/2+idTop;
-    if (idTop-40 <= scrollPosition && idBottom >= scrollPosition) {
+    if (idTop-60 <= scrollPosition && idBottom >= scrollPosition) {
       $('.wm-nav ul').find('li').removeClass('active');
       $('.wm-nav-content ul li').find('[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
     }
@@ -53,18 +52,3 @@ function scrollSection () {
 function toggleMenu () {
   $('.wm-nav-content ul').toggle(500);
 }
-
-// ver appendTo de Jquery
-
-
-/*  if(window.screen.width < 1370) {
-    console.log('Soy una pantalla pequeña');
-  }
-
-  $(window).resize(function () {
-    var size = $(this).width();
-    if(size < 768) {
-      console.log("Soy un size pequeño");
-    }
-    
-  })*/
