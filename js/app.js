1e225e1bc10d5e0bd9bcbe2ab3287e65836d2057
tamybl <tamara.bravo.l@gmail.com>
2018-03-26 15:03:33 -0300

    Maquetacion seccion colores y funcionalidad. Se agrega probador de colores

--- js/app.js
+++ js/app.js
@@ -41,19 +41,57 @@ $(document).ready( function () {
     $(this).find('span').text($(this).find('span').text() == 'Ver Menos' ? 'Ver Más' : 'Ver Menos');
   })
 
-  $('.btn-copy-clipboard').click(function () {
-    // Se copia el codigo que corresponde con el boton
-    var copyCode = $(this).parent().find('code').text();
+  $('.btn-copy-clipboard').click(function (e) {
+    e.preventDefault();
+    var prismCode = '';
+    var inputCode = '';
+    // Se copia el codigo que corresponde con el boton. Verifica si es 
+    prismCode = $(this).parent().find('code').text();
+    inputCode = $(this).parent().find('input').val();
+    if(prismCode !== '') {
+      copyCode = prismCode;
+    }
+    else {
+      copyCode = inputCode;
+    }
     //console.log(copyCode);
-    // Se crea un elemento de textarea y se 
+    // Se crea un elemento de textarea y se inserta en el body
     var textarea = document.createElement('textarea');
     document.body.appendChild(textarea);
     textarea.append(copyCode);
     textarea.select();
-    /* Copy the text inside the text field */
+    // Copia el texto copiado en el textarea
     document.execCommand("Copy");
     document.body.removeChild(textarea);
   })
+
+  $('#check-color').keyup(function () {
+    var colorBase = '#cccccc';
+    var inputColor = $(this).parent().find('input[type="text"]').val();
+    var pattern = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i;
+    if(pattern.test(inputColor)) {
+      console.log('Es un color valido');
+      $(this).parent().find('.color-box').css('background-color', inputColor);
+      $(this).parent().parent().find('#check-color').css('border', '1px solid'+inputColor);
+      if(inputColor.length === 4) {
+        var rest = inputColor.replace(/#/g , "").split("");
+        inputColor = '#'+rest[0]+rest[0]+rest[1]+rest[1]+rest[2]+rest[2];
+        $(this).parent().find('input[type="color"]').val(inputColor);
+      }
+      else {
+        $(this).parent().find('input[type="color"]').val(inputColor);
+      }
+    }
+  })
+
+  $('#check-color input[type="color"]').change(function () {
+    var color = $(this).val();
+    $(this).parent().find('.color-box').css('background-color', color);
+    $(this).parent().parent().find('#check-color').css('border', '1px solid'+color);
+    $('#check-color input[type="text"]').val(color);
+
+  })
+
   
 })
 
